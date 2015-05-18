c2b432aa8cdbe1eea57993339d8f1125c24c219d
Corvinox <rpgjinki@naver.com>
2015-05-18 17:51:07 +0900

    init

---
+++ Assets/02_Scripts/BlockObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockObject : MonoBehaviour {
+
+	Block block { get; set; }
+
+	void OnEnable () {
+		Debug.Log ("Enable!");
+	}
+
+	void OnDisable() {
+		Debug.Log ("Disable!");
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
